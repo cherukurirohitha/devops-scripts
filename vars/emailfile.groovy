b990567878250fbe238e25694056e19200cd376f
cherukurirohitha <40024833+cherukurirohitha@users.noreply.github.com>
2022-01-25 10:19:03 +0530

    Update emailfile.groovy

--- vars/emailfile.groovy
+++ vars/emailfile.groovy
@@ -1,3 +1,3 @@
-def sendemail(){
-emailext body: 'Hello Jenkins', subject: 'JenkinsTestemail', to: 'rohitha.cherukuri@gmail.com'
+def sendemail(toemail){
+  emailext body: 'Hello Jenkins', subject: 'JenkinsTestemail', to: "${toemail}"
 }
